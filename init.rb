07952b8e843ef1a4cf0e9de2ef1bf6a343b24531
Yasuhiro Kobayashi <yasuhiro.kbys@gmail.com>
2017-03-24 16:35:33 +0900

    minor fix

--- init.rb
+++ init.rb
@@ -19,6 +19,6 @@ Redmine::Plugin.register :redmine_watcher_groups do
   name 'Redmine Watcher Groups plugin'
   author 'Yasuhiro Kobayashi (original by Kamen Ferdinandov)'
   description 'This is a plugin for Redmine to add wathcer groups functionality'
-  version '1.0.0'
+  version '1.0.1'
   url 'https://github.com/ppyv/redmine_watcher_groups'
 end
